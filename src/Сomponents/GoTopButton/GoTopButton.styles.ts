10c0eac424445adaecc477fec1b593731eb54220
persikB <bloodypersik@gmail.com>
2026-05-06 16:40:38 +0300

    Изменена форма, убрана тень, добавлено изменение фона при hover

--- src/Сomponents/GoTopButton/GoTopButton.styles.ts
+++ src/Сomponents/GoTopButton/GoTopButton.styles.ts
@@ -13,17 +13,17 @@ export const GoTopButtonWrapper = styled.button`
     justify-content: center;
 
     border: none;
-    border-radius: 12px;
+    border-radius: 50%;
 
     background: #14141B;
 
     cursor: pointer;
     z-index: 1000;
 
-    transition: transform 0.2s ease, box-shadow 0.2s ease;
+    transition: transform 0.2s ease, background 0.2s ease;
 
     &:hover {
         transform: scale(1.1);
-        box-shadow: 0 0 20px rgba(255, 146, 0, 0.4);
+        background: linear-gradient(45deg, #ff6200 50%, #ffcc00 100%)
     }
 `;
